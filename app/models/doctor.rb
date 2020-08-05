4b6dedf546e240f5ff47159e2f5c75b6a10b5a53
D1yr <alexander.m.sahli@gmail.com>
2020-08-05 17:02:38 -0400

    080520 doctor and patient practice

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,9 +1,28 @@
+require 'pry'
+
+
+
+
+
+
 class Doctor
-    attr_reader :specialty
+    attr_reader :speciality
     attr_accessor :name, :years
-    def initialize(name, specialty, years=1)
+    @@all = []
+    def initialize(name, speciality, years=1)
         @name = name
-        @specialty = specialty
+        @specialty = speciality
         @years = years
+        @@all << self
+    end
+    def greet
+        puts "Hello! Welcome back, please have a seat and your Doctor will be right with you!"
+    end
+    def self.find_by_speciality(query)
+        self.all.filter {|doc| doc.specialty == query} ##solution we factored down in slack w/ ryan, kain, and anson
     end
 end
+binding.pry
+#Doctor.all: should return a list of all doctor instances
+#Doctor#greet: should print a greeting that would make any patient feel welcomed!
+#Doctor.find_by_speciality: should take a specialty string as an argument and return a list of the doctors who have that specialty 
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,33 @@
 class Patient
+    attr_reader :impatience
     attr_accessor :name, :age
-    def initialize(name, age)
+    @@all = []
+    def initialize(name, age, impatience=0)
         @name = name
         @age = specialty
+        @impatience = impatience
+        @@all << self
     end
+
+    def inquire_appt_ready
+        puts "Your doctor will be seeing you soon!"
+        @impatience -= 1
+    end
+    end
+    
+    def self.all
+        @@all
+    end
+    
+    private
+    def increase_impatience
+        @impatience += 1
+    end
+    end
+
 end
+
+#Patients should be initialized with an instance variable, impatience, set to 0
+#Patient#increase_impatience: should be a private method that increases impatience by 1 
+#Patient#inquire_appt_ready: should print that the doctor will be ready soon and increase patient impatience by 1
+#Patient.all: should return a list of all patient instances
